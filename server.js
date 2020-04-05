773b7d0b1ed1ca62604d238afb08fc1fdbcc0a16
Pratik <icanpratikpawar@gmail.com>
2020-04-05 12:00:30 +0530

    Done!

--- server.js
+++ server.js
@@ -1,116 +1,154 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const formatMessage = require('./controller/messages');
-require('./models/mongodb');
-const mongoose = require('mongoose');
-require('events').EventEmitter.prototype._maxListeners = 0;
-
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const formatMessage = require("./controller/messages");
+require("./models/mongodb");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+require("events").EventEmitter.prototype._maxListeners = 0;
+const fs = require("fs");
 
 const {
-	userJoin,
-	userLeave,
-	getRoomUsers,
-} = require('./controller/users');
+  userJoin,
+  userLeave,
+  getRoomUsers
+} = require("./controller/users");
 
 //Link To get the GroupChat Model & its Defined Schema In A Variable
 //const Group = mongoose.model('Group');
-const GroupChat = mongoose.model('GroupChat');
+const GroupChat = mongoose.model("GroupChat");
 
-var room = '';
-var username = '';
-
+var room = "";
+var username = "";
+app.use(fileUpload());
 //Get the post data from client
-app.use(express.urlencoded({
-	extended: false
-}))
-app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json());
 
 //Set View Engine To EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //Set Static Directory
-app.use(express.static(__dirname + '/views'));
 
+app.use(express.static(__dirname + "/views"));
 
 //Socket Complete Logic
-io.on('connection', (socket) => {
-	const user = userJoin(socket.id, username, room);
-	socket.join(user.room);
-	GroupChat.find({
-		roomName: user.room
-	}, (err, docs) => {
-		if (!err) {
-			socket.emit('message', docs);
-			//When User Gets Connected For The First Time
-			socket.emit('messages', formatMessage(user.username, 'Welcome To Chat App'));
-		} else {
-			console.log('Error during record insertion : ' + err);
-		}
-	});
+io.on("connection", (socket) => {
+  const user = userJoin(socket.id, username, room);
+  socket.join(user.room);
+  GroupChat.find({
+      roomName: user.room,
+    },
+    (err, docs) => {
+      if (!err) {
+        socket.emit("message", docs);
+        //When User Gets Connected For The First Time
+        socket.emit(
+          "messages",
+          formatMessage(user.username, ["Welcome To Chat App"])
+        );
+      } else {
+        console.log("Error during record insertion : " + err);
+      }
+    }
+  );
+
+  //Notify other users in group about it
+  socket.broadcast
+    .to(user.room)
+    .emit("messages", formatMessage(user.username, ["Joined The Chat"]));
+
+  // Send users and room info
+  io.to(user.room).emit("roomUsers", {
+    users: getRoomUsers(user.room),
+  });
+
+  //Get user message & respond to all users in room
+  socket.on("chatMessage", (message) => {
+    var file = message[1];
+    var fileName = message[2];
+    var flag = false
 
 
-	//Notify other users in group about it
-	socket.broadcast.to(user.room).emit('messages', formatMessage(user.username, 'Joined The Chat'));
+    if (file != undefined) {
+      var path = __dirname + "/views/img/" + fileName;
+      flag = true
+      fs.writeFile(path + "", file, function (err) {
+        if (err) throw err;
+        console.log("Saved!");
+        
+      });
 
-	// Send users and room info
-	io.to(user.room).emit('roomUsers', {
-		users: getRoomUsers(user.room)
-	});
 
-	//Get user message & respond to all users in room
-	socket.on('chatMessage', (msg) => {
-		var groupchat = new GroupChat();
-		groupchat.roomName = user.room;
-		groupchat.message = msg;
-		groupchat.from = user.username
-		groupchat.socketId = socket.id;
-		groupchat.save((err, doc) => {
-			if (!err)
-				console.log("Succesful");
-			else
-				console.log('Error during record insertion : ' + err);
-		});
+      var groupchat = new GroupChat();
+      groupchat.roomName = user.room;
+      groupchat.message = path;
+      groupchat.from = user.username;
+      groupchat.socketId = socket.id;
+      groupchat.save((err) => {
+        if (!err) console.log("Succesful!");
+        else console.log("Error during record insertion : " + err);
+      });
+    }
 
-		//Send the message received from the Users to the collection group chats
-		io.to(user.room).emit('messages', formatMessage(user.username, msg));
-	});
+    var groupchat = new GroupChat();
+    groupchat.roomName = user.room;
+    groupchat.message = message[0];
+    groupchat.from = user.username;
+    groupchat.socketId = socket.id;
+    groupchat.save((err) => {
+      if (!err) console.log("Succesful");
+      else console.log("Error during record insertion : " + err);
+    });
 
-	//When User Left the chat 
-	socket.on('disconnect', () => {
-		io.to(user.room).emit('messages', formatMessage(user.username, ' Left The Chat '));
-		userLeave(socket.id);
-		// Send users and room info
-		io.to(user.room).emit('roomUsers', {
-			users: getRoomUsers(user.room)
-		});
-	});
+    //Send the message received from the Users to the collection group chats
+    if (flag==true) {
+      io.to(user.room).emit("messages", formatMessage(user.username, [message[0],"img/"+fileName]));
+    } else {
+      console.log("bye")
+      io.to(user.room).emit("messages", formatMessage(user.username, [message[0]]));
+    }
+  });
+
+  //When User Left the chat
+  socket.on("disconnect", () => {
+    io.to(user.room).emit(
+      "messages",
+      formatMessage(user.username, " Left The Chat ")
+    );
+    userLeave(socket.id);
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      users: getRoomUsers(user.room),
+    });
+  });
 });
 
 //Code To Start The Server using http/express
 http.listen(3000, (err) => {
-	if (!err) {
-		console.log("Server Started");
-	} else {
-		console.log("Not Started");
-	}
-
+  if (!err) {
+    console.log("Server Started");
+  } else {
+    console.log("Not Started");
+  }
 });
 
-
-// Routes For Chats App
-app.get('/', (req, res) => {
-	res.render('index.ejs', {
-		topicHead: 'Language Discussion Portal'
-	});
+// Routes For Group Chat App
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    topicHead: "Language Discussion Portal",
+  });
 });
-app.post('/', (req, res) => {
-
-	room = req.body.room
-	username = req.body.username
-	res.render('chat.ejs', {
-		topicHead: 'Language Discussion Portal',
-		room: room,
-		username: username
-	});
+app.post("/", (req, res) => {
+  room = req.body.room;
+  username = req.body.username;
+  res.render("chat.ejs", {
+    topicHead: "Language Discussion Portal",
+    room: room,
+    username: username,
+  });
 });
--- views/js/main.js
+++ views/js/main.js
@@ -1,11 +1,10 @@
-const chatForm = $('#chat-form');
-const chatMessages = $('.chat-messages');
-const roomName = $('#room-name');
-
+const chatForm = $("#chat-form");
+const chatMessages = $(".chat-messages");
+const roomName = $("#room-name");
 
 const socket = io();
 
-socket.on('message', message => {
+socket.on("message", (message) => {
   //console.log(message.text);
   outputsMessage(message);
 });
@@ -16,59 +15,138 @@ socket.on('message', message => {
 // });
 
 // Get room and users
-socket.on('roomUsers', ({
+socket.on("roomUsers", ({
   users
 }) => {
-  $('#users').empty();
+  $("#users").empty();
   var items = [];
   $.each(users, (i, item) => {
-    items.push('<li>' + item.username + '</li>');
-
+    items.push("<li>" + item.username + "</li>");
   }); // close each()
 
-  $('#users').append(items.join(''));
-
+  $("#users").append(items.join(""));
 });
 
 // Message from server
-socket.on('messages', message => {
+socket.on("messages", (message) => {
   outputMessage(message);
   chatMessages.scrollTop(chatMessages.get(0).scrollHeight);
 });
 
-
 // Message submit
-chatForm.on('submit', function (e) {
+chatForm.on("submit", function (e) {
   e.preventDefault();
-
+  var file = "";
+  var fileName = "";
   //Get the input element by ID
-  const msg = $('#msg').val()
-  // Emit message to server
-  socket.emit('chatMessage', msg);
+  const msg = $("#msg").val();
 
+  //Get The File From User
+  var file = document.getElementById("myFile").files[0];
+  if (file != undefined) {
+    var fileName = document.getElementById("myFile").files[0].name;
+  }
+
+  // Emit message to server
+  socket.emit("chatMessage", [msg, file, fileName]);
+  $("#myFile").val("");
   // Clear input
-  $('#msg').val('');
+  $("#msg").val("");
   //  e.target.elements.msg.focus();
 });
 
 // Current Output messages
 function outputMessage(message) {
-  chatMessages.append(
-    $('<div class="message">').append(
-      '<p class="meta">' + message.username + '  <span>' + message.time +
-      '</span></p><p class="text">' + message.text + '</p>'));
+  if (message.text.length == 1) {
+    chatMessages.append(
+      $('<div class="message">').append(
+        '<p class="meta">' +
+        message.username +
+        "  <span>" +
+        message.time +
+        '</span></p><p class="text">' +
+        message.text[0] +
+        "</p>"
+      )
+    );
+  } else {
+    if (message.text[1].split('.')[1] != 'pdf') {
+      chatMessages.append(
+        $('<div class="message">').append(
+          '<p class="meta">' +
+          message.username +
+          "  <span>" +
+          message.time +
+          '</span></p><img src="' +
+          message.text[1] +
+          '"alt="face" height="100" width="100"><p class="+text+">' +
+          message.text[0] +
+          "</p>"
+        )
+      );
+    } else {
+      console.log(message.text[1])
+      chatMessages.append(
+        $('<div class="message">').append(
+          '<p class="meta">' +
+          message.username +
+          "<span>" +
+          message.time +
+          '</span></p><iframe src="' +
+          message.text[1] +
+          '"width="100%" height="300px"></iframe><p class="+text+">' +
+          message.text[0] +
+          "</p>"
+
+        )
+      );
+    }
+  }
 }
 
 // Previous Stored Output messages
 function outputsMessage(message) {
   items = [];
   $.each(message, (i, item) => {
-    items.push('<div class="message"><p class="meta">' + item.from + '  <span>' + item.date +
-      '</span></p><p class="text">' + item.message + '</p></div>');
+    if (item.message.split("/")[1] != "home") {
+      items.push(
+        '<div class="message"><p class="meta">' +
+        item.from +
+        "  <span>" +
+        item.date +
+        '</span></p><p class="text">' +
+        item.message +
+        "</p></div>"
+      );
+    } else {
+      var url = item.message.split("/views/")[1];
+      if (url.split('.')[1] == 'pdf') {
+        items.push(
+          '<div class="message"><p class="meta">' +
+          item.from +
+          "  <span>" +
+          item.date +
+          '</span></p><iframe src="' +
+          url +
+          '"width="100%" height="300px"></iframe></div>'
+        );
+      }
+      else{
+        items.push(
+          '<div class="message"><p class="meta">' +
+          item.from +
+          "  <span>" +
+          item.date +
+          '</span></p><img src="' +
+          url +
+          '" alt="face" height="100" width="100"></div>'
+        );
+      }
 
+    }
   }); // close each()
 
-  chatMessages.append(items.join(''));
+  chatMessages.append(items.join(""));
 }
 
 //Try After Some Time
@@ -77,6 +155,6 @@ function outputsMessage(message) {
 //   chatMessages.append(
 //     setTimeout(function() { $('<div class="message">').append(
 //       '<p class="meta">' + message.username + '  <span>' + message.time +
-//       '</span></p><p class="text">' + message.text + '</p>').fadeOut('fast'); 
+//       '</span></p><p class="text">' + message.text + '</p>').fadeOut('fast');
 //     }, 5000));
 // }
